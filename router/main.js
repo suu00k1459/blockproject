82ad5b88e3321e06b63db3b130dd55fe318995b7
suu00k1459 <suu00k1459@gmail.com>
2021-06-11 16:00:46 +0900

    update2

--- router/main.js
+++ router/main.js
@@ -6,12 +6,15 @@ module.exports = function(app) {
          extended: true
       }));
      
-     app.get('/',function(req,res){
-        res.render('index.html')
+     app.get('/',function(req,res){ //url 주소 입력 시 실행되는 내용
+        res.render('index.html') 
      });
      app.get('/about',function(req,res){
         res.render('about.html');
     });
+    app.get('/about2',function(req,res){ //url 주소 입력 시 실행되는 내용
+      res.render('about2.html') 
+   });
      
     app.get('/listall', function (req, res) {
       //res.render('about.html');
